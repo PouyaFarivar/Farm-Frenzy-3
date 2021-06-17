dae4f98007f2215f24e297ec4380781cd5c9ab5f
sh-fzl <84247191+sh-fzl@users.noreply.github.com>
2021-06-17 19:22:43 +0430

    Update Processor.java
    loger added

--- src/Processor.java
+++ src/Processor.java
@@ -1,7 +1,10 @@
 import com.google.gson.Gson;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+import java.io.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -138,6 +141,10 @@ public class Processor {
     }
 
     public void play (User user , Level level){
+        LocalDate date = LocalDate.now();
+        LocalTime time = LocalTime.now();
+        String inputLoger="";
+        ArrayList<String> logerText=new ArrayList<String>();
         Game game = new Game(level) ;
         Scanner scanner = new Scanner(System.in);
         Matcher matcher ;
@@ -152,12 +159,15 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Bought animal successfully.";
                             break;
                         case 1:
                             System.out.println("Wrong animal name.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) +" Wrong animal name.";
                             break;
                         case 2:
                             System.out.println("Not enough coins.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Not enough coins to buy the animal";
                             break;
                         default:
                             break;
@@ -169,12 +179,15 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Picked up successfully";
                             break;
                         case 1:
                             System.out.println("Not enough space in ware House.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Not enough space in ware House.";
                             break;
                         case 2:
                             System.out.println("Given coordinates is empty.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Given coordinates is empty.";
                             break;
                         default:
                             break;
@@ -184,12 +197,15 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Well filled successfully";
                             break;
                         case 1:
                             System.out.println("Well is not empty yet.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Given coordinates is empty.";
                             break;
                         case 2 :
                             System.out.println("Well is being filled.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Well is being filled.";
                             break;
                         default:
                             break;
@@ -201,12 +217,15 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Grass Planted successfully";
                             break;
                         case 1:
                             System.out.println("Not enough water.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Not enough water.";
                             break;
                         case 2 :
                             System.out.println("Given coordinate is not on the map.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Given coordinate is not on the map.";
                             break;
                         default:
                             break;
@@ -217,15 +236,19 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Built Workshop successfully";
                             break;
                         case 1:
                             System.out.println("You cant build this workShop in this level.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Workshop not available.";
                             break;
                         case 2:
-                            System.out.println("You dont have enough coins");
+                            System.out.println("You don't have enough coins");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Not enough coins.";
                             break;
                         case 3:
                             System.out.println("WorkShop already built.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " WorkShop already built.";
                             break;
                         default:
                             break;
@@ -236,18 +259,23 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Workshop working successfully";
                             break;
                         case 1:
                             System.out.println("Wrong workShop name.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Wrong workShop name.";
                             break;
                         case 2:
                             System.out.println("WorkShop not built.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " WorkShop not built.";
                             break;
                         case 3:
                             System.out.println("WorkShop is working already.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " WorkShop is working already.";
                             break;
                         case 4:
                             System.out.println("not enough entry product.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Not enough entry product for the workshop.";
                         default:
                             break;
                     }
@@ -258,12 +286,15 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Cage built successfully";
                             break;
                         case 1:
                             System.out.println("No predator at the given coordinates.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " No predator at the given coordinates.";
                             break;
                         case 2:
                             System.out.println("The Predator at the given coordinates is already captured.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Predator already captured.";
                             break;
                         default:
                             break;
@@ -279,15 +310,19 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Truck loaded successfully.";
                             break;
                         case 1:
                             System.out.println("No such product in ware house.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " No such product available.";
                             break;
                         case 2:
                             System.out.println("Not enough space in Truck inventory.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Not enough space in Truck.";
                             break;
                         case 3 :
                             System.out.println("Truck is moving.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Moving Truck.";
                             break;
                         default:
                             break;
@@ -303,44 +338,93 @@ public class Processor {
                     switch (possible) {
                         case 0:
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Truck unloaded successfully.";
                             break;
                         case 1:
                             System.out.println("No such product in Truck.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " No such product in Truck.";
                             break;
                         case 2:
                             System.out.println("Not enough space in Ware house.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Not enough space in Ware house.";
                             break;
                         case 3 :
                             System.out.println("Truck is moving.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Moving Truck.";
                             break;
                         default:
                             break;
                     }
-                }else if (action.contains("truck go")){
+                }
+                else if (action.contains("truck go")){
                     int possible = game.truckGo() ;
                     switch (possible){
                         case 0 :
                             System.out.println("Operation successful.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Truck is moving successfully.";
                             break;
                         case 1 :
                             System.out.println("Truck is empty.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Truck is empty.";
                             break;
                         case 2 :
                             System.out.println("Truck is already in move.");
+                            inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " Moving Truck.";
                             break;
                         default:
                             break;
                     }
-                } else if ((matcher= getCommandMatcher(action, "turn (\\d+)")).find()) {
-                    turn = Integer.parseInt(matcher.group(1)) ;
-                }else if (action.contains("exit")){
-                    break;
-                }else if (action.contains("inquiry")){
-                    game.plot();
-                }else{
-                    System.out.println("INVALID COMMAND.");
                 }
-            }else {
+                else if ((matcher= getCommandMatcher(action, "turn (\\d+)")).find()) {
+                    turn = Integer.parseInt(matcher.group(1)) ;
+                }
+                else if (action.contains("exit"))
+                {
+                    LocalDateTime myObj = LocalDateTime.now();
+                    BufferedWriter bw = null;
+                    for (String str : logerText)
+                    {
+                        try
+                        {
+                            File f1 = new File(user.getUserName() + "Loger.txt");
+                            if (!f1.exists())
+                            {
+                                f1.createNewFile();
+                                FileWriter myWriter = new FileWriter(f1.getName());
+                                myWriter.write("Username:" + user.getUserName() +
+                                        "\n Date Created :" + String.valueOf(myObj) + "\n");
+
+                            }
+                            FileWriter fileWritter = new FileWriter(f1.getName(), true);
+                            bw = new BufferedWriter(fileWritter);
+                            bw.write(str);
+                            bw.newLine();
+
+                        } catch (IOException e)
+                        {
+                            e.printStackTrace();
+                        }
+
+                    }
+                    try
+                    {
+                        bw.write("Date of last modification :" + String.valueOf(myObj));
+                        bw.newLine();
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    break;
+                }
+                else if (action.contains("inquiry")){
+                    game.plot();
+                }
+                else{
+                    System.out.println("INVALID COMMAND.");
+                    inputLoger="[Error] "+ String.valueOf(date) + " " + String.valueOf(time) + " INVALID COMMAND.";
+                }
+            }
+            else {
                 int possible = game.goTurn();
                 if (possible == 0) {
                     turn = turn - 1;
@@ -373,7 +457,8 @@ public class Processor {
                                 }
                             }
                         }
-                    }else {
+                    }
+                    else {
                         user.getLevelTime().put(game.getLevel().getLevelNum() , game.getTurn()) ;
                         user.setCoin(user.getCoin() + game.getCoins() - game.getLevel().getGoalCoins());
                         user.setMaximumLevel(game.getLevel().getLevelNum());
@@ -397,15 +482,19 @@ public class Processor {
                     switch (medal){
                         case 1 :
                             System.out.println("You got a Gold medal on level " + game.getLevel().getLevelNum());
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Gold Medal ";
                             break;
                         case 2 :
                             System.out.println("You got a Silver medal on level " + game.getLevel().getLevelNum());
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Silver Medal ";
                             break;
                         case 3 :
                             System.out.println("You got a Bronze medal on level " + game.getLevel().getLevelNum());
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " Bronze Medal ";
                             break;
                         case 4 :
                             System.out.println("No medal.");
+                            inputLoger="[Info] "+ String.valueOf(date) + " " + String.valueOf(time) + " No Medal ";
                             break;
                         default:
                             break;
@@ -414,6 +503,9 @@ public class Processor {
                     break;
                 }
             }
+
+            logerText.add(inputLoger);
+
         }
     }
 
