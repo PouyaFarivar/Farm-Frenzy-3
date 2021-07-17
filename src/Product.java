9762acd68f5f3ce4d22d1fd6b663f957eb03d910
sh-fzl <84247191+sh-fzl@users.noreply.github.com>
2021-07-17 16:21:03 +0430

    10. ImageView added

--- src/Product.java
+++ src/Product.java
@@ -1,3 +1,8 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.regex.Matcher;
 
 public class Product extends GamePlay
@@ -9,8 +14,11 @@ public class Product extends GamePlay
     private int time_present;
     private int available;// 0 = warehouse 1 = ground 2 = cat
     private int givenTime ;
+    private Image image;
+    private ImageView imageView;
 
-    Product(String name , int price , int x , int y , int givenTime , int size){
+    Product(String name , int price , int x , int y , int givenTime , int size) throws FileNotFoundException
+    {
         super(x , y);
         this.name = name ;
         this.price = price ;
@@ -19,6 +27,8 @@ public class Product extends GamePlay
         this.givenTime = givenTime ;
         this.size = size ;
         id = 0 ;
+        this.image= new Image(new FileInputStream("D:/project files/" + this.name + ".png"));
+        this.imageView = new ImageView(this.image);
     }
 
     Product(){}
@@ -64,51 +74,63 @@ public class Product extends GamePlay
     }
 
     static class Egg extends Product{
-        Egg(int x , int y){
+        Egg(int x , int y) throws FileNotFoundException
+        {
             super("egg" , 15 , x , y , 4 ,1);
         }
     }
     static class Feather extends Product{
-        Feather(int x , int y){
+        Feather(int x , int y) throws FileNotFoundException
+        {
             super("feather" , 20 , x , y , 4 ,1);
         }
     }
     static class Milk extends Product{
-        Milk(int x , int y){
+        Milk(int x , int y) throws FileNotFoundException
+        {
             super("milk" , 25 , x , y , 4 , 1);
         }
     }
     static class Flour extends Product{
-        Flour(int x , int y){
+        Flour(int x , int y) throws FileNotFoundException
+        {
             super("flour" , 40 , x , y , 5 , 2);
         }
     }
     static class Fabric extends Product{
-        Fabric(int x , int y){
+        Fabric(int x , int y) throws FileNotFoundException
+        {
             super("fabric" , 50 , x , y , 5 , 2);
         }
     }
     static class PacketMilk extends Product{
-    PacketMilk(int x , int y){
+    PacketMilk(int x , int y) throws FileNotFoundException
+    {
         super("packetMilk" , 60 , x, y , 5 , 2);
     }
 }
     static class Bread extends Product{
-        Bread(int x , int y){
+        Bread(int x , int y) throws FileNotFoundException
+        {
             super("bread" , 80 , x ,y , 6 , 4);
         }
     }
 
     static class Clothing extends Product{
-        Clothing(int x , int y ){
+        Clothing(int x , int y ) throws FileNotFoundException
+        {
             super("clothing" , 100 , x , y , 6 , 4);
         }
     }
     static class IceCream extends Product{
-        IceCream(int x ,  int y){
+        IceCream(int x ,  int y) throws FileNotFoundException
+        {
             super("iceCream" , 120 , x , y , 6 , 4);
         }
     }
 }
+
+
+
 
 
