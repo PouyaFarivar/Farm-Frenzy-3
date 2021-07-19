98b2b8b2b4b6e3d1ff053e6af803ffd456ffff8e
sh-fzl <84247191+sh-fzl@users.noreply.github.com>
2021-07-19 08:30:41 +0430

    5.ImageView added

--- src/Grass.java
+++ src/Grass.java
@@ -1,10 +1,22 @@
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class Grass extends GamePlay
 {
     private int health ;
+    Image image;
+    ImageView imageView;
 
-    Grass(int x  , int y ){
+    Grass(int x  , int y ) throws FileNotFoundException
+    {
         super(x , y);
         health = 5 ;
+        this.image = new Image(new FileInputStream("D:/project files/grass.png"));
+        this.imageView = new ImageView(this.image);
+
     }
 
     void getEaten(){
